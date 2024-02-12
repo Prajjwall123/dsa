6929f6d8d25bd7b387f13d7e081a13c24938c24e
Prajjwall123 <pokhrelprajwal29@gmail.com>
2024-02-12 09:42:25 +0545

    more questions

--- Two/Two_a.java
+++ Two/Two_a.java
@@ -1,5 +1,42 @@
 package Two;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Two_a {
+    public static void main(String[] args) {
+        int n = 5;
+        int[][] intervals = {{0, 2}, {1, 3}, {2, 4}};
+        int firstPerson = 0;
+
+        List<Integer> result = getPeopleWhoKnowSecret(n, intervals, firstPerson);
+        System.out.println(result);
+    }
+
+    public static List<Integer> getPeopleWhoKnowSecret(int n, int[][] intervals, int firstPerson) {
+        boolean[] knowsSecret = new boolean[n];
+        knowsSecret[firstPerson] = true;
+
+        for (int[] interval : intervals) {
+            for (int i = interval[0]; i <= interval[1]; i++) {
+                if (knowsSecret[i]) {
+                    for (int j = interval[0]; j <= interval[1]; j++) {
+                        knowsSecret[j] = true;
+                    }
+                    break;
+                }
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (knowsSecret[i]) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+
     
 }
